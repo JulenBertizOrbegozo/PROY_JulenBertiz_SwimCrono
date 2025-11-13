4c68314cfb222b870008c8c4ac73847930ec9561
JulenBertizOrbegozo <jvertizorb1@educacion.navarra.es>
2025-11-13 13:56:13 +0100

    Web_Preguntas
    Las preguntas de la web están hechas.

--- Web/SwimCrono/src/app/app.component.ts
+++ Web/SwimCrono/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'SwimCrono';
--- Web/SwimCrono/src/app/app.routes.ts
+++ Web/SwimCrono/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { FunctionalitiesComponent } from './functionalities/functionalities.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'functionalities', component: FunctionalitiesComponent },
+  { path: 'questions', component: QuestionsComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '**', redirectTo: '' }
+];
